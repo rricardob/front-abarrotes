eff43e245232a21d4d185fcfb1dee9138a129db4
Fernando Tello <fernando_tellol@hotmail.com>
2022-02-28 16:28:41 -0500

    fix content

--- src/views/content/Content.js
+++ src/views/content/Content.js
@@ -1,73 +1,104 @@
-import React from 'react'
+import React from 'react';
 
 export const Content = () => {
   return (
-    <>
+    <div className="all-content-wrapper">
       <div className="section-admin container-fluid">
         <div className="row admin text-center">
           <div className="col-md-12">
             <div className="row">
               <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                 <div className="admin-content analysis-progrebar-ctn res-mg-t-15">
-                  <h4 className="text-left text-uppercase"><b>Orders</b></h4>
+                  <h4 className="text-left text-uppercase">
+                    <b>Orders</b>
+                  </h4>
                   <div className="row vertical-center-box vertical-center-box-tablet">
                     <div className="col-xs-3 mar-bot-15 text-left">
-                      <label className="label bg-green">30% <i className="fa fa-level-up" aria-hidden="true" /></label>
+                      <label className="label bg-green">
+                        30% <i className="fa fa-level-up" aria-hidden="true" />
+                      </label>
                     </div>
                     <div className="col-xs-9 cus-gh-hd-pro">
                       <h2 className="text-right no-margin">10,000</h2>
                     </div>
                   </div>
                   <div className="progress progress-mini">
-                    <div style={{ width: '78%' }} className="progress-bar bg-green" />
+                    <div
+                      style={{ width: '78%' }}
+                      className="progress-bar bg-green"
+                    />
                   </div>
                 </div>
               </div>
-              <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12" style={{ marginBottom: 1 }}>
+              <div
+                className="col-lg-3 col-md-3 col-sm-3 col-xs-12"
+                style={{ marginBottom: 1 }}>
                 <div className="admin-content analysis-progrebar-ctn res-mg-t-30">
-                  <h4 className="text-left text-uppercase"><b>Tax Deduction</b></h4>
+                  <h4 className="text-left text-uppercase">
+                    <b>Tax Deduction</b>
+                  </h4>
                   <div className="row vertical-center-box vertical-center-box-tablet">
                     <div className="text-left col-xs-3 mar-bot-15">
-                      <label className="label bg-red">15% <i className="fa fa-level-down" aria-hidden="true" /></label>
+                      <label className="label bg-red">
+                        15%{' '}
+                        <i className="fa fa-level-down" aria-hidden="true" />
+                      </label>
                     </div>
                     <div className="col-xs-9 cus-gh-hd-pro">
                       <h2 className="text-right no-margin">5,000</h2>
                     </div>
                   </div>
                   <div className="progress progress-mini">
-                    <div style={{ width: '38%' }} className="progress-bar progress-bar-danger bg-red" />
+                    <div
+                      style={{ width: '38%' }}
+                      className="progress-bar progress-bar-danger bg-red"
+                    />
                   </div>
                 </div>
               </div>
               <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                 <div className="admin-content analysis-progrebar-ctn res-mg-t-30">
-                  <h4 className="text-left text-uppercase"><b>Revenue</b></h4>
+                  <h4 className="text-left text-uppercase">
+                    <b>Revenue</b>
+                  </h4>
                   <div className="row vertical-center-box vertical-center-box-tablet">
                     <div className="text-left col-xs-3 mar-bot-15">
-                      <label className="label bg-blue">50% <i className="fa fa-level-up" aria-hidden="true" /></label>
+                      <label className="label bg-blue">
+                        50% <i className="fa fa-level-up" aria-hidden="true" />
+                      </label>
                     </div>
                     <div className="col-xs-9 cus-gh-hd-pro">
                       <h2 className="text-right no-margin">$70,000</h2>
                     </div>
                   </div>
                   <div className="progress progress-mini">
-                    <div style={{ width: '60%' }} className="progress-bar bg-blue" />
+                    <div
+                      style={{ width: '60%' }}
+                      className="progress-bar bg-blue"
+                    />
                   </div>
                 </div>
               </div>
               <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                 <div className="admin-content analysis-progrebar-ctn res-mg-t-30">
-                  <h4 className="text-left text-uppercase"><b>Yearly Sales</b></h4>
+                  <h4 className="text-left text-uppercase">
+                    <b>Yearly Sales</b>
+                  </h4>
                   <div className="row vertical-center-box vertical-center-box-tablet">
                     <div className="text-left col-xs-3 mar-bot-15">
-                      <label className="label bg-purple">80% <i className="fa fa-level-up" aria-hidden="true" /></label>
+                      <label className="label bg-purple">
+                        80% <i className="fa fa-level-up" aria-hidden="true" />
+                      </label>
                     </div>
                     <div className="col-xs-9 cus-gh-hd-pro">
                       <h2 className="text-right no-margin">$100,000</h2>
                     </div>
                   </div>
                   <div className="progress progress-mini">
-                    <div style={{ width: '60%' }} className="progress-bar bg-purple" />
+                    <div
+                      style={{ width: '60%' }}
+                      className="progress-bar bg-purple"
+                    />
                   </div>
                 </div>
               </div>
@@ -84,22 +115,42 @@ export const Content = () => {
                   <div className="row">
                     <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                       <div className="caption pro-sl-hd">
-                        <span className="caption-subject text-uppercase"><b>Product Sales</b></span>
+                        <span className="caption-subject text-uppercase">
+                          <b>Product Sales</b>
+                        </span>
                       </div>
                     </div>
                     <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                       <div className="actions graph-rp">
                         <div className="btn-group" data-toggle="buttons">
                           <label className="btn btn-grey active">
-                            <input type="radio" name="options" className="toggle" id="option1" defaultChecked />Today</label>
+                            <input
+                              type="radio"
+                              name="options"
+                              className="toggle"
+                              id="option1"
+                              defaultChecked
+                            />
+                            Today
+                          </label>
                           <label className="btn btn-grey">
-                            <input type="radio" name="options" className="toggle" id="option2" />Week</label>
+                            <input
+                              type="radio"
+                              name="options"
+                              className="toggle"
+                              id="option2"
+                            />
+                            Week
+                          </label>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div id="curved-line-chart" className="flot-chart-sts flot-chart curved-chart-statistic" />
+                <div
+                  id="curved-line-chart"
+                  className="flot-chart-sts flot-chart curved-chart-statistic"
+                />
               </div>
             </div>
             <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
@@ -109,7 +160,10 @@ export const Content = () => {
                   <li>
                     <div id="sparklinedash" />
                   </li>
-                  <li className="text-right sp-cn-r"><i className="fa fa-level-up" aria-hidden="true" /> <span className="counter sales-sts-ctn">8659</span></li>
+                  <li className="text-right sp-cn-r">
+                    <i className="fa fa-level-up" aria-hidden="true" />{' '}
+                    <span className="counter sales-sts-ctn">8659</span>
+                  </li>
                 </ul>
               </div>
               <div className="white-box analytics-info-cs mg-b-30">
@@ -118,7 +172,10 @@ export const Content = () => {
                   <li>
                     <div id="sparklinedash2" />
                   </li>
-                  <li className="text-right"><i className="fa fa-level-up" aria-hidden="true" /> <span className="counter sales-sts-ctn">7469</span></li>
+                  <li className="text-right">
+                    <i className="fa fa-level-up" aria-hidden="true" />{' '}
+                    <span className="counter sales-sts-ctn">7469</span>
+                  </li>
                 </ul>
               </div>
               <div className="white-box analytics-info-cs mg-b-30">
@@ -127,7 +184,10 @@ export const Content = () => {
                   <li>
                     <div id="sparklinedash3" />
                   </li>
-                  <li className="text-right"><i className="fa fa-level-up" aria-hidden="true" /> <span className="counter sales-sts-ctn">6011</span></li>
+                  <li className="text-right">
+                    <i className="fa fa-level-up" aria-hidden="true" />{' '}
+                    <span className="counter sales-sts-ctn">6011</span>
+                  </li>
                 </ul>
               </div>
               <div className="white-box analytics-info-cs">
@@ -136,14 +196,16 @@ export const Content = () => {
                   <li>
                     <div id="sparklinedash4" />
                   </li>
-                  <li className="text-right"><i className="fa fa-level-down" aria-hidden="true" /> <span className="sales-sts-ctn">18%</span></li>
+                  <li className="text-right">
+                    <i className="fa fa-level-down" aria-hidden="true" />{' '}
+                    <span className="sales-sts-ctn">18%</span>
+                  </li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
-
-  )
-}
+    </div>
+  );
+};
