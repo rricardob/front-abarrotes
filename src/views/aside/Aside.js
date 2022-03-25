031114046a3b764deb4610dfcc3cfbf78b738eb8
ricardo <rbalbis2001@gmail.com>
2022-03-24 23:05:01 -0500

    resolve conficts

--- src/views/aside/Aside.js
+++ src/views/aside/Aside.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import React from 'react'
-import { AsideData } from './../../components/aside/AsideData'
-
-=======
 import React from 'react';
 import { Link } from 'react-router-dom';
->>>>>>> master
 
 export const Aside = () => {
   return (
@@ -80,76 +74,6 @@ export const Aside = () => {
                 </div>
               </a>
             </div>
-<<<<<<< HEAD
-            <ul className="nav">
-                {/*<li className="nav-item profile">
-                    <div className="profile-desc">
-                        <div className="profile-pic">
-                            <div className="count-indicator">
-                                <img className="img-xs rounded-circle " src="assets/images/faces/face15.jpg" />
-                                <span className="count bg-success" />
-                            </div>
-                            <div className="profile-name">
-                                <h5 className="mb-0 font-weight-normal">Henry Klein</h5>
-                                <span>Gold Member</span>
-                            </div>
-                        </div>
-                        <a href="#" id="profile-dropdown" data-toggle="dropdown"><i className="mdi mdi-dots-vertical" /></a>
-                        <div className="dropdown-menu dropdown-menu-right sidebar-dropdown preview-list" aria-labelledby="profile-dropdown">
-                            <a href="#" className="dropdown-item preview-item">
-                                <div className="preview-thumbnail">
-                                    <div className="preview-icon bg-dark rounded-circle">
-                                        <i className="mdi mdi-settings text-primary" />
-                                    </div>
-                                </div>
-                                <div className="preview-item-content">
-                                    <p className="preview-subject ellipsis mb-1 text-small">Account settings</p>
-                                </div>
-                            </a>
-                            <div className="dropdown-divider" />
-                            <a href="#" className="dropdown-item preview-item">
-                                <div className="preview-thumbnail">
-                                    <div className="preview-icon bg-dark rounded-circle">
-                                        <i className="mdi mdi-onepassword  text-info" />
-                                    </div>
-                                </div>
-                                <div className="preview-item-content">
-                                    <p className="preview-subject ellipsis mb-1 text-small">Change Password</p>
-                                </div>
-                            </a>
-                            <div className="dropdown-divider" />
-                            <a href="#" className="dropdown-item preview-item">
-                                <div className="preview-thumbnail">
-                                    <div className="preview-icon bg-dark rounded-circle">
-                                        <i className="mdi mdi-calendar-today text-success" />
-                                    </div>
-                                </div>
-                                <div className="preview-item-content">
-                                    <p className="preview-subject ellipsis mb-1 text-small">To-do list</p>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-    </li>*/}
-                <li className="nav-item nav-category">
-                    <span className="nav-link">Navegacion</span>
-                </li>
-                {
-                    AsideData.map((val, key) => {
-                        return (
-                        <li className="nav-item menu-items" key={key} onClick={() => {window.location.pathname = val.link}}>
-                            <a className="nav-link">
-                                <span className="menu-icon">
-                                    <i className={val.icon} />
-                                </span>
-                                <span className="menu-title">{val.title}</span>
-                            </a>
-                        </li>
-                        )
-                    })
-                }
-
-=======
           </div>
         </li>
         <li className="nav-item nav-category">
@@ -277,7 +201,6 @@ export const Aside = () => {
                   Register{' '}
                 </a>
               </li>
->>>>>>> master
             </ul>
           </div>
         </li>
