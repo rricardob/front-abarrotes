df186b2e44f43a1c4064e455254b176a7a81c3d1
ricardo <rbalbis2001@gmail.com>
2022-07-22 12:06:21 -0500

    add endpoints for cliente, vendedor and categoria

--- src/views/config/index.js
+++ src/views/config/index.js
@@ -1,9 +1,9 @@
 export const PATH = process.env.REACT_APP_PATH
 
-//endpoints product
+//endpoints
 
 export const PRODUCT = {
-    GET: 'producto/findAll',
+    GET:    'producto/findAll',
     CREATE: 'producto/create',
     UPDATE: 'producto/update',
     DELETE: 'producto/delete'
@@ -11,7 +11,21 @@ export const PRODUCT = {
 
 export const CATEGORY = {
     GET: 'categoria/findAll',
-    CREATE: '',
-    UPDATE: '',
-    DELETE: ''
+    CREATE: 'categoria/create',
+    UPDATE: 'categoria/update',
+    DELETE: 'categoria/delete'
+}
+
+export const CLIENTE = {
+    GET:    'cliente/findAll',
+    CREATE: 'cliente/create',
+    UPDATE: 'cliente/update',
+    DELETE: 'cliente/delete'
+}
+
+export const VENDEDOR = {
+    GET:    'vendedor/findAll',
+    CREATE: 'vendedor/create',
+    UPDATE: 'vendedor/update',
+    DELETE: 'vendedor/delete'
 }
