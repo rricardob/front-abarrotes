9bd475c668902254e2d0430cbbb84d094b0d9648
ricardo <rbalbis2001@gmail.com>
2022-07-25 12:53:48 -0500

    in progress

--- src/views/producto/CrudTableRowProducto.jsx
+++ src/views/producto/CrudTableRowProducto.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const CrudTableRow = ({ el, setDataToEdit, deleteData }) => {
-  let { co_id, co_fecha, cliente, vendedor, estado, co_total } = el;
-  let stateClass = estado === 0 ? "badge badge-success" : "badge badge-danger";
+  let { co_id, co_fecha, cliente, vendedor, eliminado, co_total } = el;
+  let stateClass = eliminado === 0 ? "badge badge-success" : eliminado === 1 ? "badge badge-danger" : "badge badge-warning";
 
   return (
     <tr>
@@ -12,14 +12,14 @@ const CrudTableRow = ({ el, setDataToEdit, deleteData }) => {
       <td>{vendedor}</td>      
       <td className='text-center'> 
         <label className={stateClass}>
-          {estado === 0 ? "ACTIVO" : "INACTIVO"}
+          {eliminado === 0 ? "INACTIVO" : eliminado === 1 ? "ACTIVO" : "ANULADO"}
         </label> 
       </td>
       <td>{co_total}</td>
       <td className='text-center'>
         <button type="button" className="btn btn-primary btn-sm" onClick={() => setDataToEdit(el)}>Editar</button>&nbsp;
         <button type="button" className="btn btn-danger btn-sm" onClick={() => deleteData(el)}>Eliminar</button>&nbsp;
-        <button type="button" className="btn btn-info btn-sm" onClick={() => deleteData(el)}>Anular</button>&nbsp;
+        <button type="button" className="btn btn-warning btn-sm" onClick={() => deleteData(el)}>Anular</button>&nbsp;
         <button type="button" className="btn btn-success btn-sm" onClick={() => deleteData(el)}>Visualizar</button>
       </td>
     </tr>
