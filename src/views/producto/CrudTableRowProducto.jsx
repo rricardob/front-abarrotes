808a70622863bed21016a78e143065555640f9d9
ricardo <rbalbis2001@gmail.com>
2022-07-25 13:20:23 -0500

    in progress

--- src/views/producto/CrudTableRowProducto.jsx
+++ src/views/producto/CrudTableRowProducto.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import moment from 'moment';
+
+
 
 const CrudTableRow = ({ el, setDataToEdit, deleteData }) => {
   let { co_id, co_fecha, cliente, vendedor, eliminado, co_total } = el;
@@ -7,7 +10,7 @@ const CrudTableRow = ({ el, setDataToEdit, deleteData }) => {
   return (
     <tr>
       <td>{co_id}</td>
-      <td>{co_fecha}</td>
+      <td>{moment(co_fecha).format('DD/MM/yyyy')}</td>
       <td>{cliente}</td>
       <td>{vendedor}</td>      
       <td className='text-center'> 
