1f074039d553988fc54337a7c8ceb5cf9b269a3f
rricardob <ricardo_23_91@hotmail.com>
2022-07-29 01:22:42 -0500

    end filter component

--- src/views/comprobante/Comprobante.js
+++ src/views/comprobante/Comprobante.js
@@ -35,6 +35,52 @@ export function Comprobante() {
 
   const filter = (params) => {
     console.log("filter -> ",params)
+
+    let url = rootpath + SALE_RECEIPT.GET + 
+    "?cliente="+params.cl_id+"&vendedor="+params.ve_id+"&estado="+params.estado+"&fecinicio="+params.f_ini+"&fecfin="+params.f_fin;
+
+    /*if (params.cl_id !== null && params.cl_id !== undefined) {
+      url += "?cliente="+params.cl_id
+    }else{
+      url +="?cliente=''"
+    }
+
+    if (params.ve_id !== null && params.ve_id !== undefined) {
+      url += "&vendedor="+params.ve_id
+    }else{
+      url +="&vendedor=''"
+    }
+
+    if (params.estado !== null && params.estado !== undefined) {
+      url += "&estado="+params.estado
+    }else{
+      url +="&estado=''"
+    }
+
+    if (params.f_ini !== null && params.f_ini !== undefined) {
+      url += "&fecinicio="+params.f_ini
+    }else{
+      url +="&fecinicio=''"
+    }
+
+    if (params.f_fin !== null && params.f_fin !== undefined) {
+      url += "&fecfin="+params.f_fin
+    }else{
+      url +="&fecfin=''"
+    }*/
+
+
+    api.get( url )
+    .then(res => {
+      setDb(res)
+    })
+    .catch(err => {
+      console.error(err)
+      setError(err)
+      setDb(null)
+    })
+
+
   }
 
   const deleteData = params => {
